e5b01ed4234e88a6f2310c377d43fe8bebb8103f
Dennis Byberg <dennis.byberg@hotmail.com>
2025-04-13 16:28:00 +0200

    chore(provision_vm.sh): update resource group and environment variables for new demo application deployment style(provision_vm.sh): format az commands for better readability and maintainability

--- azure/provision_vm.sh
+++ azure/provision_vm.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-resource_group=DockerDemoRG
-location=northeurope
-env_name=DockerDemoEnv
-app_name=cloudapplicationadvanced
-app_port=8080
-image=ghcr.io/campus-molndal-cloh24/cloud-applications-advanced:latest
+#---------- GLOBAL VARIABLES -------------
+resource_group="DemoMerchStore"
+location="swedencentral"
+env_name="DemoMerchStoreEnv"
+app_name="demomerchstoreapp"
+app_port="8080"
+image="ghcr.io/campus-molndal-cloh24/cloud-applications-advanced:latest"
 
-az group create --location $location --name $resource_group
+az group create \
+    --location $location \
+    --name $resource_group
 
-az containerapp env create --name $env_name --resource-group $resource_group --location $location
+az containerapp env create \
+    --name $env_name \
+    --resource-group $resource_group \
+    --location $location
 
-az containerapp create --name $app_name --resource-group $resource_group \
+az containerapp create \
+    --name $app_name \
+    --resource-group $resource_group \
     --image $image \
     --environment $env_name \
     --target-port $app_port \
